d9f533a56f6958d3ba73f6e7a6c9ef0a6963d9b2
Yuxuan Ma <yuxuan.maaa@gmail.com>
2025-05-24 18:51:20 -0400

    v0.1.0 test

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div style={{ width: '300px', padding: '20px' }}>
+      <h1>GreenCard Buddy</h1>
+      <p>Welcome to GreenCard Buddy!</p>
+    </div>
+  );
+};
+
+export default App; 
